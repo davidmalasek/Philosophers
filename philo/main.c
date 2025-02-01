b10655251c90e556ed9dd74095132427bb5faccb
David Malášek <davidmalasek@post.cz>
2025-02-01 14:44:37 +0100

    Added Philosophers

---
+++ philo/main.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davidmalasek <davidmalasek@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/01 14:39:26 by davidmalase       #+#    #+#             */
+/*   Updated: 2025/02/01 14:44:10 by davidmalase      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * argv[1] - number_of_philosophers
+ * argv[2] - time_to_die
+ * argv[3] - time_to_eat
+ * argv[4] - time_to_sleep
+ * argv[5] - number_of_times_each_philosopher_must_eat (optional)
+ */
+int	main(int argc, char **argv)
+{
+}
